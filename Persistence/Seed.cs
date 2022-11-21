2e38b232ddece0f7a8a383f26c5aabbad99852df
haruna <harunaabaldeh@gmail.com>
2022-11-21 00:44:23 -1200

    Added identity

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,11 +1,27 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName = "Bob", UserName = "bob", Email = "bob@test.com"},
+                    new AppUser{DisplayName = "Tom", UserName = "tom", Email = "tom@test.com"},
+                    new AppUser{DisplayName = "Jane", UserName = "jane", Email = "jane@test.com"},
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Documents.Any()) return;
 
             var documents = new List<Document>
@@ -61,9 +77,6 @@ namespace Persistence
                     PlaceFound = "Bakau New Town",
                     DateFound = DateTime.UtcNow
                 },
-
-
-
             };
 
             await context.Documents.AddRangeAsync(documents);
